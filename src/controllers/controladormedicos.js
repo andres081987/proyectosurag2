c862a73e11d6a56c415f02c6d40dbc997ff898cd
andres081987 <andres08.1987@gmail.com>
2022-11-30 17:34:48 -0500

    formularios

--- src/controllers/controladormedicos.js
+++ src/controllers/controladormedicos.js
@@ -1,21 +1,22 @@
+//IMPORTACIONES
+import {capturarDatos} from '../formularios/formularioMedicos.js'
+
 //etiquetas que controle:
 let etiquetaNombre=document.getElementById("nombres")
 let etiquetaBoton=document.getElementById("botonenvio")
 let etiquetaDocumento=document.getElementById("documento")
+let etiquetaEspecialidad=document.getElementById("especialidad")
+let etiquetaRegistro=document.getElementById("registro")
+let etiquetaCorreo=document.getElementById("correo")
+let etiquetaSede=document.getElementById("sede")
+let etiquetaHorario=document.getElementById("horario")
+let etiquetaDescripcion=document.getElementById("descripcion")
+let etiquetaFoto=document.getElementById("foto")
 
 //detectar el evento de clic
 etiquetaBoton.addEventListener("click",function(evento){
 
     evento.preventDefault()   
-    // Swal.fire(
-    //     'Exito!',
-    //     'El especialista ha sido registrado!',
-    //     'success'
-    //   )
-
-    //recibiendo los datos del formulario
-    let nombresMedico=etiquetaNombre.value
-    console.log(nombresMedico)
-    let documentoMedico=etiquetaDocumento.value
-    console.log(documentoMedico)
+    capturarDatos(etiquetaNombre,etiquetaDocumento,etiquetaEspecialidad,etiquetaRegistro,etiquetaCorreo,etiquetaSede,etiquetaHorario,etiquetaDescripcion,etiquetaFoto)
+       
 })
---
+++ src/formularios/formularioMedicos.js
@@ -0,0 +1,32 @@
+import {validarFormulario} from './validacionFormularioMedico.js'
+
+// Creamos una funcion que nos permita recibir los datos del formulario
+export function capturarDatos(etiquetaNombre,etiquetaDocumento,etiquetaEspecialidad,etiquetaRegistro,etiquetaCorreo,etiquetaSede,etiquetaHorario,etiquetaDescripcion,etiquetaFoto){  
+      
+let nombreMedico=etiquetaNombre.value
+let documentoMedico=etiquetaDocumento.value
+let especialidadMedico=etiquetaEspecialidad.value
+let registroMedico=etiquetaRegistro.value
+let correoMedico=etiquetaCorreo.value
+let sedeMedico=etiquetaSede.value
+let horarioMedico=etiquetaHorario.value
+let descripcionMedico=etiquetaDescripcion.value
+let fotoMedico=etiquetaFoto.value
+
+// Debemos almacenar todos los valores del formulario en una sola variable
+//Obetjo=almacenar varios datos en 1soloespacio de memoria
+let datosFormularioMedico={
+    nombre:nombreMedico,
+    documento:documentoMedico,
+    especialidad:especialidadMedico,
+    registro:registroMedico,
+    correo:correoMedico,
+    sede:sedeMedico,
+    horario:horarioMedico,
+    descripcion:descripcionMedico,
+    foto:fotoMedico
+}
+console.log(datosFormularioMedico)
+
+
+}

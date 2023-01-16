d9c357ffd51fe32c60a70931daa586722c7ade7b
andres081987 <andres08.1987@gmail.com>
2023-01-16 17:21:16 -0500

    Se crea vistas controlador pintar medico

---
+++ services/consultaMedicos.js
@@ -0,0 +1,20 @@
+export async function consultarMedicos(){
+    //consumiendo API
+    //1. CONOCER LA DIRECCION DEL API O SERVICIO
+    let url="https://apigestorapp.vercel.app/gestionapp/especialistas"
+
+    //2. configurar a que voy al API
+    //CONFIGURANDO LA REQUEST O PETICION
+    let peticion={
+        method:"GET",        
+    }
+
+    //3. CONSUMIR
+    //TRAER LOS DATOS DEL API
+    let respuesta=await fetch(url,peticion)
+    let medicos=await respuesta.json()
+
+    return(medicos)
+
+
+}
